9a83f690fa951274b64e0dcd618ff93c0243ee1f
Christian Howard <choward1491@gmail.com>
2017-09-29 21:34:24 -0700

    added new data and script to generate data for part 1.2. Last commit did not properly have these items tracked.

---
+++ MP1/scripts/gatherData1p2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+bin="../bin"
+tiledir="${bin}/tiles"
+datadir="../data"
+
+methods=('astar')
+mazes=('tinySearch' 'smallSearch' 'mediumSearch')
+heuristic="chull"
+
+for planner in ${methods[@]};
+do
+	out_dir="${datadir}/${planner}"
+	mkdir -p ${out_dir}
+	for maze in ${mazes[@]};
+	do
+		echo "Starting data collection for $maze using $planner..."
+		${bin}/./mp_exec -maze "${bin}/${maze}.txt" \
+			-agent_t "${tiledir}/vader.png" \
+			-wall_t "${tiledir}/background.png"\
+			-back_t "${tiledir}/obstacle.png"\
+			-goal_t "${tiledir}/goal.png"\
+			-out_gif "${out_dir}/${planner}_${maze}.gif"\
+			-out "${out_dir}/${planner}_${maze}_path.txt"\
+			-stats "${out_dir}/${planner}_${maze}_stats.txt"\
+			-p $planner -h $heuristic
+		echo "... Complete."
+	done
+done
