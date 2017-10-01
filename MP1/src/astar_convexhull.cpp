3957ef75f658e121387642a23af0bb338a6b6077
Christian Howard <choward1491@gmail.com>
2017-09-30 19:53:42 -0500

    Cleaned up Convex Hull Heuristic and ensured it is admissible.

--- MP1/src/astar_convexhull.cpp
+++ MP1/src/astar_convexhull.cpp
@@ -226,34 +226,92 @@ namespace astar {
             }
             
             // compute convex hull based distance measurement
-            int chull_dist = 0;
-            double avg_edge_len = 0.0;
+            double chull_dist       = 0;
+            double avg_edge_len     = 0.0;
+            const double beta       = 0.01;
+            unsigned int num_interior_goals = 0;
             unsigned int len = unvisited_pts.size();
+            unsigned int pidx= len - 1;
             unvisited_pts.push_back(point1);
+            std::vector<bool> on_hull(unvisited_pts.size(),false);
+            
             if( unvisited_pts.size() > 1 ){
                 // compute convex hull around 2D point cloud
                 get_chull( unvisited_pts, chull_edges );
                 
                 // compute perimeter around convex hull
-                const double inv_ne = 1.0/((double)(chull_edges.size()-1));
-                const double inv_sq2 = 1.0 / sqrt(2.0);
-                unsigned int max_dist = 0;
-                double dist = 0;
+                bool agent_on_hull = true;
+                double dist = 0, max_dist1 = 0, max_dist2 = 0, min_dist = 1e30;
+                
                 for(unsigned int i = 0; i < chull_edges.size(); ++i){
+                    
+                    // get edge on convex hull
                     auto edge = chull_edges[i];
-                    dist = dist_between_pts(unvisited_pts[edge.first], unvisited_pts[edge.second]);
-                    if( /*(edge.first == len || edge.second == len ) &&*/ dist > max_dist ){ max_dist = dist; }
-                    chull_dist += (int)dist;
-                    avg_edge_len += (1.0*dist*inv_sq2*inv_ne);
+                    
+                    // set that ith edge's first vertex is on the convex hull
+                    on_hull[edge.first] = true;
+                    
+                    // compute weighted L1 and L2 distance between vertices on edge
+                    dist =  beta*dist_between_pts(unvisited_pts[edge.first], unvisited_pts[edge.second]) +
+                            (1.0 - beta)*dist_between_pts(unvisited_pts[edge.first], unvisited_pts[edge.second],true);
+                    
+                    // if the edge has the agent's position vertex, update max edge distance
+                    // and set the flag to say the agent is on the convex hull
+                    if( edge.first == pidx || edge.second == pidx ){ agent_on_hull = true; max_dist1 = dist > max_dist1 ? dist : max_dist1; }
+                    
+                    // else, compute the relative distance from the ith edge's first vertex
+                    // to the agent's position and update the minimum distance to agent variable
+                    else{
+                        double rel_dist = beta*dist_between_pts(unvisited_pts[edge.first], unvisited_pts[pidx]) +
+                                        (1.0 - beta)*dist_between_pts(unvisited_pts[edge.first], unvisited_pts[pidx],true);;
+                        min_dist = min_dist > rel_dist ? rel_dist : min_dist;
+                    }
+                    
+                    // compute maximum edge length within convex hull
+                    max_dist2 = dist > max_dist2 ? dist : max_dist2;
+                    
+                    // update the convex hull with the current edge length
+                    chull_dist += dist;
                 }
                 
-                // subtract largest edge distance since an agent would not go around the whole perimeter
-                chull_dist -= max_dist;
-                avg_edge_len -= (1.0*(max_dist+1)*inv_sq2*inv_ne);
+                // if agent is on the convex hull, remove the largest edge distance the agent sits on
+                if( agent_on_hull ) { chull_dist -= max_dist1;              }
+                
+                // else remove the largest edge on the convex hull and add the minimal distance
+                // from the agent to a point on the convex hull
+                else                { chull_dist += (min_dist - max_dist2); }
+                
+                // set the number of goals on the interior of the convex hull
+                num_interior_goals = unvisited_pts.size() - (chull_edges.size() - agent_on_hull);
             }
             
+            
+            // compute minimum distance between interior points to chull point
+            // and between interior points to other interior points
+            bool setDistance = false;
+            double min_interior_dist = 1e30;
+            for(unsigned int i = 0; i < unvisited_pts.size(); ++i){
+                if( !on_hull[i] ){
+                    for(unsigned int j = 0; j < unvisited_pts.size(); ++j){
+                        if( on_hull[j] ){
+                            double rel_dist = beta*dist_between_pts(unvisited_pts[i], unvisited_pts[j]) +
+                                            (1.0 - beta)*dist_between_pts(unvisited_pts[i], unvisited_pts[j],true);;
+                            min_interior_dist = min_interior_dist > rel_dist ? rel_dist : min_interior_dist;
+                            setDistance = true;
+                        }else if( i != j ){
+                            double rel_dist = beta*dist_between_pts(unvisited_pts[i], unvisited_pts[j]) +
+                                            (1.0 - beta)*dist_between_pts(unvisited_pts[i], unvisited_pts[j],true);;
+                            min_interior_dist = min_interior_dist > rel_dist ? rel_dist : min_interior_dist;
+                            setDistance = true;
+                        }
+                    }// end for
+                }// end if
+            }// end for
+            
+            if( !setDistance ){ min_interior_dist = 1.0; }
+            
             // compute overall distance estimate based on convex hull and penalization of unvisited goals
-            cost = (int)( chull_dist + scale_*(num_unvisited - (chull_edges.size()-1)) + avg_edge_len*(chull_edges.size()-1) );
+            cost = ( chull_dist + (scale_*min_interior_dist)*static_cast<double>(num_interior_goals) );
             
         }else{ custom::exception("A* Convex Hull Distance Heuristic does not have a reference maze, cannot compute distance properly."); }
         return cost;
